ef0d591a33f0567db7111c1b67e2f3908b753645
Experiment2601 <aniketgavas18@gmail.com>
2022-10-25 22:04:32 +0530

    Employee_Wage_UC3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,13 @@ package com.bridelabz.employeewage;
 
 public class EmployeeWage {
     int FULL_TIME = 1;
+    int PART_TIME = 2;
     int EMP_PER_HOUR_RATE = 50;
     int empHrs = 0;
     int empWage = 0;
 
+
+    //Check Employee IS present Or Absent
     public void empCheckPresentOrAbsent() {
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
@@ -15,6 +18,7 @@ public class EmployeeWage {
             System.out.println("Employee is Absent");
         }
     }
+
 
 
     //Calculate Daily EmployeeWage
@@ -27,13 +31,29 @@ public class EmployeeWage {
             empHrs = 0;
         }
         empWage = empHrs * EMP_PER_HOUR_RATE;
-        System.out.println("Daily EmployeeWage:"+empWage);
+        System.out.println("Employee Wage:"+empWage);
     }
 
 
+
+    //Add Part Time And EmployeeWage
+
+    public void addPartTimeAndEmpWage(){
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == FULL_TIME)
+            empHrs = 8;
+        else if(empCheck == PART_TIME)
+            empHrs = 4;
+        else
+            empHrs = 0;
+        empWage = empHrs * EMP_PER_HOUR_RATE;
+        System.out.println("Employee Wage: "+empWage);
+    }
+    
     public static void main(String[] args) {
         EmployeeWage emp = new EmployeeWage();
         emp.empCheckPresentOrAbsent();
         emp.empDailyWage();
+        emp.addPartTimeAndEmpWage();
     }
 }
